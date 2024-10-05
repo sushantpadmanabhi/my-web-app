478f4d90c44675a1cdf7bd3015bab30f4ded070e
sushantpadmanabhi <125899954+sushantpadmanabhi@users.noreply.github.com>
2024-10-05 10:31:56 -0400

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@ document.getElementById('dataForm').addEventListener('submit', async function (e
 
     const name = document.getElementById('name').value;
 
-    const response = await fetch('http://<20.244.37.108>:3000/data', {
+    const response = await fetch('http://20.244.37.108:3000/data', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
